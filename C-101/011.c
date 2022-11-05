708787a249caf9f04052d2a0f7aba8b54aad6b8b
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-06 00:08:29 +0300

    Add files via upload

---
+++ C-101/011.c
@@ -0,0 +1,50 @@
+//coded by vyslcbn
+//Project011
+//Array
+
+#include <stdio.h>
+
+//EX 1
+
+//int main(){
+//	int myNumbers[] = {25, 50, 75, 100};
+//	printf("%d", myNumbers[0]);
+//	
+//	return 0;
+//}
+//_________________________________________
+
+//EX 2
+
+//int main(){
+//	int myNumbers[] = {25, 50, 75, 100};
+//	 
+//	 int i;
+//	 for (i = 0; i < 3; i++){
+//	 	printf("%d\n", myNumbers[i]);
+//	 }
+//   return 0;
+//}
+//__________________________________________
+
+//EX 3
+
+int main(){
+	int myNumbers[4];
+	
+	myNumbers[0] =25 ;
+	myNumbers[1] =50 ;
+	myNumbers[2] =75 ;
+	myNumbers[3] =100;
+	
+	printf("%d\n", myNumbers[2]);
+	
+	return 0;
+}
+
+
+
+
+
+
+
