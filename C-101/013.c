3196c00d4021bd558c26c3b3d769647d51af08b7
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-06 00:11:13 +0300

    Add files via upload

---
+++ C-101/013.c
@@ -0,0 +1,18 @@
+//coded by vyslcbn
+//Project013
+//input
+
+#include <stdio.h>
+
+int main(){
+	char firstName[30];
+	
+	printf("Enter your first name;\n");
+	
+	scanf("%s", firstName);
+	
+	printf("Hello %s.", firstName);
+
+	return 0;
+}
+
