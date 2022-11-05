4bdca267cc0935f7f692762e27cda15b25d4037e
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-06 00:10:28 +0300

    Add files via upload

---
+++ C-101/012.c
@@ -0,0 +1,14 @@
+//coded by vyslcbn
+//Project012
+//Strings
+
+#include <stdio.h>
+
+int main(){
+	char greetings[] = "Hello World";
+	greetings[0] = 'V';
+	printf("%s", greetings);
+	
+	return 0;
+}
+
