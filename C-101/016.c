4417cdbfe658397a0369507ac247fd1868d5e178
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-07 15:48:13 +0300

    Add files via upload

---
+++ C-101/016.c
@@ -0,0 +1,15 @@
+//coded by vyslcbn
+//Project016
+//return values
+
+#include <stdio.h>
+
+int myFunction(int x, int y){
+	return x + y;
+}
+
+int main(){
+	printf("Result is: %d", myFunction(5, 3));
+	return 0;
+}
+
