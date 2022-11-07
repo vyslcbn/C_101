31cf4e2fe1232f8d24f5179496038cb1a228a443
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-07 15:48:50 +0300

    Add files via upload

---
+++ C-101/017.c
@@ -0,0 +1,21 @@
+//coded by vyslcbn
+//Project017
+//Math.h
+
+//sqrt kok alma
+//ceil  yuvarlama yukari
+//floor yuvarlama assagi
+//pow uss alma
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	printf("%f\n", sqrt(16));
+	printf("%f\n", ceil(1.4));
+	printf("%f\n", floor(1.4));
+	printf("%f\n", pow(3 , 4));
+	
+	return 0;
+}
+
