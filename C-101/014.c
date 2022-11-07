a936549093b645447adeb2c087814a33f49c1e26
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2022-11-07 15:47:13 +0300

    Add files via upload

---
+++ C-101/014.c
@@ -0,0 +1,17 @@
+//coded by vyslcbn
+//Project014
+//Functions
+
+#include <stdio.h>
+
+void myFunction(char name []){
+		printf("Hello %s\n", name);
+	}
+int main(){
+	myFunction("Veysel");
+	myFunction("Muhammed");
+	myFunction("World");
+			
+	return 0;
+}
+
