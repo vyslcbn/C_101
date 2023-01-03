96efc4aa1f4f6081831029645ca732037e7382ac
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2023-01-03 17:12:36 +0300

    Add files via upload

---
+++ C-101/029.c
@@ -0,0 +1,30 @@
+//coded by vyslcbn
+//Project0
+//Bir dizinin ortalamasýný bulan program
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int dizi[100], n, toplam =0;
+	float ortalama;
+	int i;
+	
+	printf("Lutfen dizinin eleman sayisini girin: ");
+	scanf("%d",&n);
+	
+	printf("Lutfen dizinin elemanlarini girin: ");
+	for (i = 0; i < n; i++){
+		
+		scanf("%d", &dizi[i]);
+		toplam += dizi[i];
+	}
+	
+	ortalama = (float)toplam / n;
+	printf("Dizinin ortalamasý: %f\n", ortalama);
+	
+	return 0;
+	
+}
+
+
