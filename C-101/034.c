6ab451efbbfdfb6a1ea7f592e54f0a22cdc6df3c
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2023-01-22 11:40:06 +0300

    Add files via upload

---
+++ C-101/034.c
@@ -0,0 +1,21 @@
+//coded by vyslcbn
+//Project034
+//girile sayýnýn karesini bulan program
+
+#include <stdio.h>
+
+int main(){
+	int sayi;
+	
+	printf("Sayiyi giriniz :");
+	scanf("%d", &sayi);
+	
+	int kare = sayi * sayi;
+	
+	printf("Girilen sayinin karesi : %d", kare);
+	
+	return 0;
+}
+
+
+
