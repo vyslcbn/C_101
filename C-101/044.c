7ff0a3416dfb30d02bf0b905db281b7e724e4ecb
Muhammed Veysel ÇOBAN <109880065+vyslcbn@users.noreply.github.com>
2023-01-24 22:56:32 +0300

    Add files via upload

---
+++ C-101/044.c
@@ -0,0 +1,16 @@
+//coded by vyslcbn
+//Project044
+//Dosya Kapatma
+
+
+#include <stdio.h>
+
+main(){
+	FIlE *fp;
+	fp =("example.txt","r");
+	
+	//Dosya icerigi okuma islemleri
+	fclose(fp);
+}
+
+
